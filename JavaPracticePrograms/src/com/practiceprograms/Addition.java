32f4ac66f213b400085132871fc4d939497f4bd5
Sachin Deshpande <sachin.deshpande1989@gmail.com>
2023-04-14 16:18:40 +0530

    Addition program added

---
+++ JavaPracticePrograms/src/com/practiceprograms/Addition.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.practiceprograms;
+
+import java.util.Scanner;
+
+/**
+ * @author Sachin Deshpande
+ *
+ */
+public class Addition {
+
+	/**
+	 * @param args
+	 */
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter two numbers for addition");
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		System.out.format("Addition of %d and %d is %d ",num1, num2, num1 + num2);
+	}
+
+}
